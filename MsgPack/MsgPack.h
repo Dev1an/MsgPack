ed4f4c99152a540c9f7c00c794ea2ebaa0d0940a
Damiaan Dufaux <damiaan@dpro.be>
2017-07-29 11:44:03 +0200

    Initial Commit

---
+++ MsgPack/MsgPack.h
@@ -0,0 +1,19 @@
+//
+//  MsgPack.h
+//  MsgPack
+//
+//  Created by Damiaan on 29/07/17.
+//  Copyright © 2017 dPro. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MsgPack.
+FOUNDATION_EXPORT double MsgPackVersionNumber;
+
+//! Project version string for MsgPack.
+FOUNDATION_EXPORT const unsigned char MsgPackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MsgPack/PublicHeader.h>
+
+
